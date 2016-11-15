61a5067feaacdb700a49c6df63624b518581e67b
Paradox <contact@paradoxdesign.co>
2016-11-15 11:45:48 -0800

    Initial Commit

---
+++ Web Service Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example
+//
+//  Created by Jay Mutzafi on 11/15/16.
+//  Copyright © 2016 Paradox Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
